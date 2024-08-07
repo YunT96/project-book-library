3c3fe6368331a13e485c0b42f1e60cf72fd64f33
YunT96 <163807078+YunT96@users.noreply.github.com>
2024-08-07 16:09:07 +0800

    Set up initial project files

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const myLibrary = [];
+
+    function Book() {
+        // the constructor...
+    }
+
+    function addBookToLibrary() {
+        // do stuff here
+    }
+
+    console.log('DOM fully loaded and parsed');
+});
